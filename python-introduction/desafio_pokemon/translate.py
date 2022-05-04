7c64befa759bf749f16d951eefff766aee2915c2
oportusgonzalo <gonzalo@datawheel.us>
2022-05-03 21:06:29 -0400

    Adds translation script (translates name to es)

---
+++ python-introduction/desafio_pokemon/translate.py
@@ -0,0 +1,14 @@
+from get_module import get_info
+
+'''Esta función toma un nombre y url, ya sea de pokemon, tipo, o especie; genera una consulta a la API y 
+retorna el valor de dicho nombre en español.'''
+
+def translate(name, url):
+    data = get_info(url)
+    translated = [data['names'][element]['name'] for element in range(0, len(data['names'])) if data['names'][element]['language']['name'] == 'es']
+    return translated[0]
+
+if __name__ == "__main__":
+    name = 'fire'
+    url = f'https://pokeapi.co/api/v2/type/{name}'
+    print(translate(name, url))
