95a936484b3a6ddd04046c4d385c5c9afa00771f
oportusgonzalo <oportusgonzalo@gmail.com>
2022-09-16 13:11:05 -0300

    Fixes bug on sql

--- sql/insert_tables.sql
+++ sql/insert_tables.sql
@@ -1,8 +1,8 @@
 --Insertando valores en tabla: Artista
-\copy artista from '/Users/gonzalooportus/Documents/CODE/data-science-desafio-latam/sql/Artista.csv' delimiter ',';
+\copy artista(nombre_artista, fecha_de_nacimiento, nacionalidad) from '/Users/gonzalooportus/Documents/CODE/data-science-desafio-latam/sql/Artista.csv' delimiter ',' csv header;
 
 --Insertando valores en tabla: album
-\copy album from '/Users/gonzalooportus/Documents/CODE/data-science-desafio-latam/sql/Album.csv' delimiter ',';
+\copy album(titulo_album, artista, anio) from '/Users/gonzalooportus/Documents/CODE/data-science-desafio-latam/sql/Album.csv' delimiter ',' csv header;
 
 --Insertando valores en tabla: cancion
-\copy cancion from '/Users/gonzalooportus/Documents/CODE/data-science-desafio-latam/sql/Cancion.csv' delimiter ',';
+\copy cancion(titulo_cancion, artista, album, numero_del_track) from '/Users/gonzalooportus/Documents/CODE/data-science-desafio-latam/sql/Cancion.csv' delimiter ',' csv header;
